b8d469eefa819bb53a777b1d2be38f32951550d5
artsel <artys74@mail.ru>
2022-10-21 14:51:57 +0300

    fakeDB add

--- src/components/user.js
+++ src/components/user.js
@@ -1,18 +1,47 @@
 import React from "react";
-const usersDB = ['Art','Begenot','Dry'];
+/*
+const usersDBFull = [
+{id:1, username:'Art', firstname:'Serge', lastname:'Artikhovich',year:'1974'},
+{id:2, username:'Begenot', firstname:'Boris', lastname:'Beginner',year:'1980'},
+{id:3, username:'Dry', firstname:'Diana', lastname:'Dry',year:'1996'}
+];
+
+*/
+const usersDBFull = [
+{id:1, username:"Art", firstname:"Serge", lastname:"Artikhovich", year:1974 },
+{id:2,username:"Begenot",firstname:"Boris",lastname:"Beginner",year:1980},
+{id:3,username:"Dry",firstname:"Diana",lastname:"Dry",year:1996}
+];
+
+
+  const validChildrenToRender = {
+    string: 'string',
+    number: 12,
+    element: <div>an element!</div>,
+    funcThatReturnsValidChild: () => <div>a component!</div>,
+  }
+
+const usersDB = ["Art", "Begenot", "Dry"];
 
 export default function User(){
-
     return(
         <div className="user">
             <ul className="list-users-items">
 
-            {usersDB.map((username, index) =>
+            {usersDBFull.map((user) =>
                     <li 
-                        key={index}
-                        className="useritem"
+                        key={user.id}
+                        className="user-item"
                         >
-                            {username}
+                        <div className="title">
+                            <span className="id">{user.id}: </span>
+                            <span className="username">{user.username}</span>
+                            <span className="year">( {user.year} )</span>
+                        </div>
+                        <div className="names">
+                            <span className="firstname">{user.firstname}</span>
+                            <span className="lastname">{user.lastname}</span>
+                        </div>
                     </li>
                 )
             }</ul>
